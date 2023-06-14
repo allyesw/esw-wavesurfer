ab3d265d90de18bb258cda565a046afe355177bf
allyesw <lunaalexandra24@yahoo.com>
2023-06-13 17:10:52 -0700

    Update wixCodeAndInstructions.js

--- wixCodeAndInstructions.js
+++ wixCodeAndInstructions.js
@@ -5,7 +5,9 @@
 
 // First, get the correct URL from Soundcloud's embed code. Towards the beginning of the code, it will say "src=" then a long URL in quotes. Enter that URL in the dataset, not including the quotes.
 
-// Next, get the Wavesurfer files from the GitHub repo here: https://github.com/allyesw/esw-wavesurfer (see readme there for upload instructions)
+// Next, get the Wavesurfer files from the GitHub repo here: https://github.com/allyesw/esw-wavesurfer (see readme there for upload instructions.) Copy the path and append to this link: https://allyesw.github.io/esw-wavesurfer/
+// EXAMPLE: https://allyesw.github.io/esw-wavesurfer/SamplePacks/NuVintage/NV-01-1x4.html
+// Then enter that URL into the dataset.
 
 // Lastly, replace all the IDs below with the correct ones. Keep everything wrapped in the onReady for the dataset. Paste into Wix page code.
 
